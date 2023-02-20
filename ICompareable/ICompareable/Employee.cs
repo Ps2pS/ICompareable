dd148a80cf7eae53031c298635895d0dbae1b06b
Pragya Singh <48428695+Ps2pS@users.noreply.github.com>
2023-02-20 23:39:07 +0530

    Code uploaded

---
+++ ICompareable/ICompareable/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICompareable
+{
+   public class Employee : IComparable<Employee>
+    {
+        public int EmpId;
+        public string EmpName;
+        public int EmpAge;
+
+        public Employee(int EmpId, string EmpName, int EmpAge)
+        {
+            this.EmpId = EmpId;
+            this.EmpName = EmpName;
+            this.EmpAge = EmpAge;
+        }
+
+        public int CompareTo(Employee other)
+        {
+            if (this.EmpAge < other.EmpAge)
+            {
+                return 1;
+            }
+            else if (this.EmpAge > other.EmpAge)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
+ 
---
+++ ICompareable/ICompareable/Program.cs
@@ -0,0 +1,54 @@
+﻿using ICompareable;
+
+namespace Program
+{
+    class IComExample
+    {
+        public static void Main()
+        {
+
+            Employee Emp1 = new Employee(1, "Pragya",30);
+            Employee Emp2 = new Employee(2, "Kanak",35);
+            int state = Emp1.CompareTo(Emp2);
+            if (state == 1)
+            {
+                Console.WriteLine("Emp1 is older than Emp2");
+            }
+            else if (state == -1)
+            {
+                Console.WriteLine("Emp2 is older than Emp1");
+            }
+            else
+            {
+                Console.WriteLine("Emp1 & Emp2 are of same age");
+            }
+            int state1 = Emp2.CompareTo(Emp1);
+            if (state1 == 1)
+            {
+                Console.WriteLine("Emp1 is older than Emp2");
+            }
+            else if (state1 == -1)
+            {
+                Console.WriteLine("Emp2 is older than Emp1");
+            }
+            else
+            {
+                Console.WriteLine("Emp1 & Emp2 are of same age");
+            }
+            int state2 = Emp1.CompareTo(Emp1);
+            if (state2 == 1)
+            {
+                Console.WriteLine("Emp1 is older than Emp2");
+            }
+            else if (state2== -1)
+            {
+                Console.WriteLine("Emp2 is older than Emp1");
+            }
+            else
+            {
+                Console.WriteLine("Both are of same age");
+            }
+
+        }
+    }
+}
